6f76e2ebf2ac7609492fa29a72a2ae4b2a473d31
Luke Huang <38710547+lukewhuang@users.noreply.github.com>
2019-04-26 11:07:16 -0700

    3D Modular Game Environment Updated

---
+++ 3DModGame/Assets/Scripts/CraneAttack.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CraneAttack : MonoBehaviour {
+
+	private Animator anim;
+	public bool left;
+	public bool right;
+
+	// Use this for initialization
+	void Start () {
+		anim = GetComponent<Animator> ();
+		left = false;
+		right = false;
+
+	}
+
+	// Update is called once per frame
+	void Update () {
+		if(left)
+			anim.SetBool ("attackLeft", true);
+		else
+			anim.SetBool ("attackLeft", false);
+		
+		if(right)
+			anim.SetBool ("attackRight", true);
+		else
+			anim.SetBool ("attackRight", false);
+	}
+
+
+}
---
+++ 3DModGame/Assets/Scripts/SendMessageToCrane.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SendMessageToCrane : MonoBehaviour {
+
+	public CraneAttack crane;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Debug.Log (crane.left);
+	}
+
+	void OnTriggerEnter(Collider other){
+		if(gameObject.tag=="collideLeft")
+			crane.left = true;
+		if(gameObject.tag=="collideRight")
+			crane.right = true;
+	}
+
+	void OnTriggerExit(Collider other){
+		if(gameObject.tag=="collideLeft")
+			crane.left = false;
+		if(gameObject.tag=="collideRight")
+			crane.right = false;
+	}
+}
